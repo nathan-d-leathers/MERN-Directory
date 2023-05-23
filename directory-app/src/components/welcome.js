07d1fb1c0debaf8e1815fda52a4663dd6683bb83
wasadmin <wasadmin@wasadmin.com>
2023-05-23 14:17:58 -0500

    added Welcoem componenet

---
+++ directory-app/src/components/welcome.js
@@ -0,0 +1,15 @@
+import {React} from React;
+
+function Welcome() {
+
+    return (
+        <div>
+            <h1>Welcome to the Travelers Employee Directory</h1>
+            <br/>
+            <link type="button">Login</link>
+            <link type="button">Create Account</link>
+        </div>
+    )
+}
+
+export default Welcome
