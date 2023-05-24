337461abeee56852a4df635d2a5939270de54ca8
wasadmin <wasadmin@wasadmin.com>
2023-05-24 13:42:08 -0500

    added registration inputs

--- directory-app/src/components/Register.js
+++ directory-app/src/components/Register.js
@@ -9,6 +9,47 @@ function Register() {
         <div>
             <h1>Welcome to the Register Account Page</h1>
             <br/>
+            <form action="/user/register" method='POST'/>
+                <label>
+                    Name:
+                    <input type="text" name="name" required/>
+                </label>
+            <br/>
+                <label>
+                    Phone Number:
+                    <input type="number" name="contact" required/>
+                </label>
+            <br/>
+                <label>
+                    Job Role:
+                    <input type="radio" id='employee' name="role" value="Employee"/>
+                    <label for="employee">Employee</label><br/>
+                    <input type="radio" id='humanResources' name="role" value="HumanResources"/>
+                    <label for="humanResources">Human Resources</label><br/>
+                    <input type="radio" id='manager' name="role" value="Manager"/>
+                    <label for="manager">Manager</label><br/>
+                </label>
+            <br/>
+                <label>
+                    Work Location:
+                    <input type="text" name="location" required/>
+                </label>
+            <br/>
+                <label>
+                    Salary:
+                    <input type="number" name="salary" required/>
+                </label>
+            <br/>
+                <label>
+                    Username:
+                    <input type="text" name="userName" required/>
+                </label>
+            <br/>
+                <label>
+                    Password:
+                    <input type="password" name="password" required/>
+                </label>
+            <br/>
             <button onClick={() => navigate('../directory')}>Go To Directory</button>
             <br/>
             <a onClick={() => navigate('../login')}>Go to Login</a>
