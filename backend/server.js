3ce8caa56368190cb25be16ecf638bf83e5e7e4c
wasadmin <wasadmin@wasadmin.com>
2023-05-24 09:03:29 -0500

    commented out some files

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,61 @@
-const express=require("express");
-const cors = require("cors");
-const dao = require("./mongo-dao");
-const app=express();
-const port=7500
+const express = require("express");
+const dotenv = require("dotenv")
 
-app.use(cors())
-app.use(express.json())
 
-app.get("api/workers", (req,res) => {
-    dao.getAllWorkers((error,workers) => {
-        if (workers) {
-            res.send(workers)
-        } else {
-            res.statusCode = 500
-            res.end()
-        }
-    });
-});
+const app = express();
 
-app.get('/hello',(req,res)=> {res.send("Hello from the back end server")});
-    //app.use(express.static("build"))
-    // app.get("*",(req,res)=>{
-    //     res.sendFile(path.resolve(__dirname,"build","index.html"));
-    // })
+dotenv.config()
+
+const port = process.env.PORT || 6000
+
+app.get("/hello", (req,res) => {
+    res.send("Hello from the backend server")
+})
 
 app.listen(port,()=> {
     console.log(`Server is running on port ${port}`);
-})
+});
 
+// const cors = require("cors");
+// const mongodb = require("mongodb")
+
+// const workers = require("./api/workers.route")
+// const dao = require("./mongo-dao");
+
+// const MongoClient = mongodb.MongoClient
+// app.use(cors())
+// app.use(express.json())
+
+// app.get("api/workers", (req,res) => {
+//     dao.getAllWorkers((error,data) => {
+//         if (data) {
+//             res.send(data)
+//         } else {
+//             res.statusCode = 500
+//             res.end()
+//         }
+//     });
+// });
+
+// app.use("/api/workers", workers)
+// app.use("*", (req,res) => res.status(404).json({error: "not found"}));
+
+
+
+// MongoClient.connect(
+//     process.env.WORKERDATA_DB_URI,
+//     {
+//         poolSize: 50,
+//         wtimeoutMS: 2500,
+//         useNewUrlParse: true
+//     }
+//     )
+//     .catch(error => {
+//         console.error("Could not connect to MongoDB", error)
+//         process.exit(1)
+//     })
+//     .then(async client => {
+//         app.listen(port,()=> {
+//             console.log(`Server is running on port ${port}`);
+//         })
+//     })
